364c774cb490d21164afce33ec93830cff5fcc49
Mimzaaa <din.tan1995l@mail.ru>
2024-08-30 17:32:11 +0300

    +allure-report

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from testdata.DataProvider import DataProvider
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 @pytest.fixture
